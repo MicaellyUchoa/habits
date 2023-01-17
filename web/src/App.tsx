225a5642c64bc461f72ee960d303b85f67508a19
MicaellyUchoa <micaellyit@hotmail.com>
2023-01-17 10:04:54 -0300

    feat: created setup on web, mobile, backend

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import { Habits } from './components/Habits'
+
+function App() {
+
+  return (
+    <>
+      <Habits completed={1} />
+      <Habits completed={2} />
+      <Habits completed={3} />
+      <Habits completed={4} />
+      <Habits completed={5} />
+      <Habits completed={6} />
+    </>
+  )
+}
+
+export default App
