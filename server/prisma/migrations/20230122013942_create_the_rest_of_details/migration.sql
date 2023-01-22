950cf4abab111af6b27d26a87fe26ee3e60ae3a6
MicaellyUchoa <micaellyit@hotmail.com>
2023-01-21 22:41:17 -0300

    feat: create migrations

---
+++ server/prisma/migrations/20230122013942_create_the_rest_of_details/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - You are about to drop the `habits` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "habits";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "habit_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "habit_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "DayHabit" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "days_date_key" ON "days"("date");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DayHabit_day_id_habit_id_key" ON "DayHabit"("day_id", "habit_id");
